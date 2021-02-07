61803e78b10cdda287fbb0184b880da799d26f54
Jordan Hayes <jordanhayes@jordans-mbp.wlan.rose-hulman.edu>
2021-02-07 18:03:43 -0500

    main page (most likely) contains all items added by everyone

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -37,6 +37,7 @@ rhit.FB_KEY_SCHEDULE = "schedule";
 rhit.fbAuthManager = null;
 rhit.fbUserManager = null;
 rhit.fbUserItemManager = null;
+rhit.fbAllItemManager = null
 
 /**
  * 
@@ -248,6 +249,43 @@ rhit.FbUserItemManager = class {
 	  }
 }
 
+rhit.FbAllItemManager = class {
+	static CATEGORIES = ["BOOKS", "ELECTRONICS", "FOOD", "FURNITURE", "OTHER"];
+
+	constructor() {
+		this.documents = [];
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_ITEMS);
+    	this._unsubscribe = null;
+	}
+
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.onSnapshot((querySnapshot) => {
+		  this._documentSnapshots = querySnapshot.docs;
+		  changeListener();
+		});
+	  }
+	
+	  stopListening() {
+		this._unsubscribe();
+	  }
+
+	get length() {
+		return this._documentSnapshots.length;
+	}
+
+	getItemAtIndex(index) {
+		const docSnapshot = this._documentSnapshots[index];
+		const item = new rhit.Item(
+		  docSnapshot.id,
+		  docSnapshot.get(rhit.FB_KEY_NAME),
+		  docSnapshot.get(rhit.FB_KEY_DESCRIPTION),
+		  docSnapshot.get(rhit.FB_KEY_CATEGORY),
+		  docSnapshot.get(rhit.FB_KEY_PRICE)
+		);
+		return item;
+	  }
+}
+
 /**
  * 
  * 
@@ -273,6 +311,59 @@ rhit.MainPageController = class {
 		document.querySelector("#logout").onclick = (event) => {
 			rhit.fbAuthManager.signOut();
 		}
+
+		rhit.fbAllItemManager.beginListening(this.updateList.bind(this));
+	}
+
+	updateList() {
+		const newList = htmlToElement('<div id="itemRow" class="row"> </div>');
+
+		for (let i = 0; i < rhit.fbAllItemManager.length; i++) {
+			const item = rhit.fbAllItemManager.getItemAtIndex(i);
+			const newCard = this._createCard(item);
+		
+			// newCard.onclick = (event) => {
+			//   console.log(`You clicked on ${mq.id}`);
+			//   // rhit.storage.setMovieQuoteId(mq.id);
+		
+			//   window.location.href = `/moviequote.html?id=${mq.id}`;
+			// };
+		
+			newList.appendChild(newCard);
+		}
+
+		// remove old quoteListContainer
+		const oldList = document.querySelector("#itemRow");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+	
+		// put in new quoteListContainer
+		oldList.parentElement.appendChild(newList);
+	}
+
+	_createCard(item) {
+		return htmlToElement(`
+		<div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top"
+                  data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+                  alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block"
+                  src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22290%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20290%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17730751f77%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A15pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17730751f77%22%3E%3Crect%20width%3D%22290%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2294.4453125%22%20y%3D%22119.1%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+                  data-holder-rendered="true" />
+                <div class="card-body">
+                  <p class="card-text">${item.name} - $${item.priceRange.low} - ${item.priceRange.high}</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm">
+                        More Details
+                      </button>
+					</div>
+					<span class="material-icons"> favorite_border </span>
+                  </div>
+                </div>
+              </div>
+			</div>
+		`);
 	}
 }
 
@@ -562,6 +653,7 @@ rhit.initializePage = function () {
 	if (document.querySelector("#mainList")) {
 		console.log("You are on the list page.");
 		const uid = urlParams.get("uid");
+		rhit.fbAllItemManager = new rhit.FbAllItemManager();
 		new rhit.MainPageController(uid);
 	}
 
