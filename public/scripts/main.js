a626f5774f1616968020862c37ff2257f38a8050
Jordan Hayes <jordanhayes@jordans-mbp.wlan.rose-hulman.edu>
2021-02-07 17:56:35 -0500

    individual user sees items in my items page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -36,7 +36,7 @@ rhit.FB_KEY_PRICE = "price";
 rhit.FB_KEY_SCHEDULE = "schedule";
 rhit.fbAuthManager = null;
 rhit.fbUserManager = null;
-rhit.fbItemManager = null;
+rhit.fbUserItemManager = null;
 
 /**
  * 
@@ -193,7 +193,7 @@ rhit.FbUserManager = class {
 	}
 }
 
-rhit.FbItemManager = class {
+rhit.FbUserItemManager = class {
 	static CATEGORIES = ["BOOKS", "ELECTRONICS", "FOOD", "FURNITURE", "OTHER"];
 
 	constructor() {
@@ -205,21 +205,47 @@ rhit.FbItemManager = class {
 	add(name, description, priceRange, category) {
 		this._ref.add({
 			[rhit.FB_KEY_ITEM_NAME]: name,
-			[rhit.FB_KEY_CATEGORY]: rhit.FbItemManager.CATEGORIES[category - 1],
+			[rhit.FB_KEY_CATEGORY]: rhit.FbUserItemManager.CATEGORIES[category - 1],
 			[rhit.FB_KEY_DESCRIPTION]: description,
 			[rhit.FB_KEY_PRICE]: priceRange,
 			[rhit.FB_KEY_SELLER]: rhit.fbAuthManager.uid
 		}).then(function (docRef) {
 			console.log("Document written in ID: ", docRef.id);
+		  }).
+		  then(() => {
+			window.location.href = '/my-item.html';
 		  })
 		  .catch(function (error) {
 			console.error("Error adding document: ", error);
 		  });
 	}
 
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.where(rhit.FB_KEY_SELLER, "==", rhit.fbAuthManager.uid).onSnapshot((querySnapshot) => {
+		  this._documentSnapshots = querySnapshot.docs;
+		  changeListener();
+		});
+	  }
+	
+	  stopListening() {
+		this._unsubscribe();
+	  }
+
 	get length() {
 		return this._documentSnapshots.length;
 	}
+
+	getItemAtIndex(index) {
+		const docSnapshot = this._documentSnapshots[index];
+		const item = new rhit.Item(
+		  docSnapshot.id,
+		  docSnapshot.get(rhit.FB_KEY_NAME),
+		  docSnapshot.get(rhit.FB_KEY_DESCRIPTION),
+		  docSnapshot.get(rhit.FB_KEY_CATEGORY),
+		  docSnapshot.get(rhit.FB_KEY_PRICE)
+		);
+		return item;
+	  }
 }
 
 /**
@@ -298,6 +324,58 @@ rhit.MyItemPageController = class {
 		document.querySelector("#addItemBtn").addEventListener("click", (event) => {
 			window.location.href = "/new-item.html";
 		});
+
+		rhit.fbUserItemManager.beginListening(this.updateList.bind(this));
+	}
+
+	updateList() {
+		const newList = htmlToElement('<div id="itemRow" class="row"> </div>');
+
+		for (let i = 0; i < rhit.fbUserItemManager.length; i++) {
+			const item = rhit.fbUserItemManager.getItemAtIndex(i);
+			const newCard = this._createCard(item);
+		
+			// newCard.onclick = (event) => {
+			//   console.log(`You clicked on ${mq.id}`);
+			//   // rhit.storage.setMovieQuoteId(mq.id);
+		
+			//   window.location.href = `/moviequote.html?id=${mq.id}`;
+			// };
+		
+			newList.appendChild(newCard);
+		}
+
+		// remove old quoteListContainer
+		const oldList = document.querySelector("#itemRow");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+	
+		// put in new quoteListContainer
+		oldList.parentElement.appendChild(newList);
+	}
+
+	_createCard(item) {
+		return htmlToElement(`
+		<div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top"
+                  data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+                  alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block"
+                  src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22290%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20290%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17730751f77%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A15pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17730751f77%22%3E%3Crect%20width%3D%22290%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2294.4453125%22%20y%3D%22119.1%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+                  data-holder-rendered="true" />
+                <div class="card-body">
+                  <p class="card-text">${item.name} - $${item.priceRange.low} - ${item.priceRange.high}</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm">
+                        More Details
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+			</div>
+		`);
 	}
 }
 
@@ -347,7 +425,7 @@ rhit.AddItemPageController = class {
 
 			console.log(name, description, category, priceRange);
 
-			rhit.fbItemManager.add(name, description, priceRange, category);
+			rhit.fbUserItemManager.add(name, description, priceRange, category);
 		});
 	}
 }
@@ -499,13 +577,13 @@ rhit.initializePage = function () {
 
 	if (document.querySelector("#myItemsPage")) {
 		console.log('You are on the my items page');
-		rhit.fbItemManager = new rhit.FbItemManager();
+		rhit.fbUserItemManager = new rhit.FbUserItemManager();
 		new rhit.MyItemPageController();
 	}
 
 	if (document.querySelector("#newItemPage")) {
 		console.log('You are on the new item page');
-		rhit.fbItemManager = new rhit.FbItemManager();
+		rhit.fbUserItemManager = new rhit.FbUserItemManager();
 		new rhit.AddItemPageController();
 	}
 
