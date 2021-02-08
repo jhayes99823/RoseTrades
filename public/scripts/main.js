8af42c46a2014166f93b596ce24f84ccc8d8a60d
Jordan Hayes <jordanhayes@jordans-mbp.wlan.rose-hulman.edu>
2021-02-08 15:58:22 -0500

    merge with main and update account controller

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -487,50 +487,6 @@ rhit.MainPageController = class {
 	}
 }
 
-rhit.AccountPageController = class {
-	constructor() {
-		console.log('im the account page controller');
-
-		document.querySelector("#logout").addEventListener("click", (event) => {
-			rhit.fbAuthManager.signOut();
-		});
-
-		rhit.fbUserManager.beginListening(rhit.fbAuthManager.uid, this.updateView.bind(this));
-	}
-
-	updateView() {
-		let name = rhit.fbUserManager.name.split(" ");
-
-		const savedFirstName = name[0];
-		const savedLastName = name[name.length - 1];
-
-		const firstNameInput = document.querySelector("#firstNameInput");
-		const lastNameInput = document.querySelector("#lastNameInput");
-
-		firstNameInput.value = savedFirstName;
-		lastNameInput.value = savedLastName;
-
-		firstNameInput.addEventListener("input", (event) => {
-			if ((firstNameInput.value != savedFirstName) || (lastNameInput.value != savedLastName)) {
-				$("#updateBtn").attr("disabled", false);
-			} else {
-				$("#updateBtn").attr("disabled", true);
-			}
-		});
-
-		lastNameInput.addEventListener("input", (event) => {
-			if ((lastNameInput.value != savedLastName) || (firstNameInput.value != savedFirstName)) {
-				$("#updateBtn").attr("disabled", false);
-			} else {
-				$("#updateBtn").attr("disabled", true);
-			}
-		});
-
-		document.querySelector("#updateBtn").addEventListener('click', (event) => {
-			rhit.fbUserManager.updateName(firstNameInput.value + " " + lastNameInput.value);
-		});
-	}
-}
 
 rhit.MyItemPageController = class {
 	constructor() {
